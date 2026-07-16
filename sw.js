c0877d96aed167519b0f2e40a467d88b096768b8
mark606300 <mark606300@gmail.com>
2026-07-17 03:34:03 +0800

    發佈 iPhone HTTPS 試玩版

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE='paramecium-singularity-v2';
+const BASE=new URL('./',self.location.href).pathname;
+const CORE=[BASE,`${BASE}manifest.webmanifest`,`${BASE}apple-touch-icon.svg`,`${BASE}favicon.svg`];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if (event.request.method!=='GET') return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match(BASE))));
+});
